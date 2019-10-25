ad864da7f68325ac3838825c1bcd99ca7339bedc
Zach  Smith <zdsmith001@gmail.com>
2019-10-25 20:35:18 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -36,11 +36,11 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
