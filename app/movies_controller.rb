5b49340cba60562893f87f1ad587611fde406b3c
Zach  Smith <zdsmith001@gmail.com>
2019-10-25 20:39:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -48,8 +48,7 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(:id)
-  end
+  "SELECT * FROM movies WHERE id = 1"
 end
 
 def can_find_by_multiple_attributes
